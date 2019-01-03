d9d80462f7f37e2369de0e1e2422dd0a4e9739d7
Clark Rushing <crushing05@gmail.com>
2019-01-03 10:26:46 -0700

    Start lecture 6

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -3,5 +3,21 @@ pb <- read.csv("data-raw/SurvAF_PublishedKnowledge.csv")
 
 pb$Subpopulation <- toupper(letters[1:nrow(pb)])
 
+bbs <- BBS.tenstop::get_BBS10()
 
-usethis::use_data(overwrite = TRUE)
+bbs_routes <- dplyr::filter(bbs$routes, BCR %in% c(9, 10, 16))
+
+bbs_routes <- dplyr::select(bbs_routes, countrynum, statenum, Route, Active, Latitude, Longitude, BCR, RouteTypeID, routeID)
+
+bbs_weather <- dplyr::filter(bbs$weather, routeID %in% bbs_routes$routeID)
+
+bbs_weather <- dplyr::select(bbs_weather, countrynum, statenum, Route, RPID, Year, Month, Day, ObsN, StartTemp, EndTemp, StartWind, EndWind, StartSky, EndSky, StartTime, EndTime, Assistant, RunType, routeID)
+
+bbs_counts <- dplyr::filter(bbs$counts, routeID %in% bbs_routes$routeID)
+
+bbs_counts <- dplyr::select(bbs_counts, Year, aou, speciestotal, routeID)
+bbs_data <- list(routes = bbs_routes, weather = bbs_weather, counts = bbs_counts)
+
+
+usethis::use_data(bbs_data,overwrite = TRUE)
+
